c4141725f41b62f78c5762d2d1d86ffbc14cb994
eden <eden.quan.chen@gmail.com>
2024-09-03 00:05:17 +0800

    init project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gitlab.lainuoniao.cn/eden-quan/protoc-gen-go-sql-fx
+
+go 1.21.6
+
+require google.golang.org/protobuf v1.32.0
---
+++ utils/pathconv.go
@@ -0,0 +1,116 @@
+package utils
+
+import (
+	"fmt"
+	"strings"
+
+	"google.golang.org/protobuf/compiler/protogen"
+)
+
+// ConvertPath 将 alpha.beta.omega 转换为 Alpha.Beta.Omega
+func ConvertPath(p string) string {
+	if p == "" || p == "*" {
+		return ""
+	}
+	names := strings.Split(p, ".")
+	for i, n := range names {
+		names[i] = CamelCase(n)
+	}
+
+	return strings.Join(names, ".")
+}
+
+func ConvertPathWithGet(p string) string {
+	if p == "" || p == "*" {
+		return ""
+	}
+
+	names := strings.Split(p, ".")
+	for i, n := range names {
+		names[i] = "Get" + CamelCase(n) + "()"
+	}
+	return strings.Join(names, ".")
+
+}
+
+// CamelCase returns the CamelCased name.
+// If there is an interior underscore followed by a lower case letter,
+// drop the underscore and convert the letter to upper case.
+// There is a remote possibility of this rewrite causing a name collision,
+// but it's so remote we're prepared to pretend it's nonexistent - since the
+// C++ generator lowercase names, it's extremely unlikely to have two fields
+// with different capitalization.
+// In short, _my_field_name_2 becomes XMyFieldName_2.
+func CamelCase(s string) string {
+	if s == "" {
+		return ""
+	}
+	t := make([]byte, 0, 32)
+	i := 0
+	if s[0] == '_' {
+		// Need a capital letter; drop the '_'.
+		t = append(t, 'X')
+		i++
+	}
+	// Invariant: if the next letter is lower case, it must be converted
+	// to upper case.
+	// That is, we process a word at a time, where words are marked by _ or
+	// upper case letter. Digits are treated as words.
+	for ; i < len(s); i++ {
+		c := s[i]
+		if c == '_' && i+1 < len(s) && isASCIILower(s[i+1]) {
+			continue // Skip the underscore in s.
+		}
+		if isASCIIDigit(c) {
+			t = append(t, c)
+			continue
+		}
+		// Assume we have a letter now - if not, it's a bogus identifier.
+		// The next word is a sequence of characters that must start upper case.
+		if isASCIILower(c) {
+			c ^= ' ' // Make it a capital letter.
+		}
+		t = append(t, c) // Guaranteed not lower case.
+		// Accept lower case sequence that follows.
+		for i+1 < len(s) && isASCIILower(s[i+1]) {
+			i++
+			t = append(t, s[i])
+		}
+	}
+	return string(t)
+}
+
+// Is c an ASCII lower-case letter?
+func isASCIILower(c byte) bool {
+	return 'a' <= c && c <= 'z'
+}
+
+// Is c an ASCII digit?
+func isASCIIDigit(c byte) bool {
+	return '0' <= c && c <= '9'
+}
+
+func ProtocVersion(gen *protogen.Plugin) string {
+	v := gen.Request.GetCompilerVersion()
+	if v == nil {
+		return "(unknown)"
+	}
+	var suffix string
+	if s := v.GetSuffix(); s != "" {
+		suffix = "-" + s
+	}
+	return fmt.Sprintf("v%d.%d.%d%s", v.GetMajor(), v.GetMinor(), v.GetPatch(), suffix)
+}
+
+const DeprecationComment = "// Deprecated: Do not use."
+
+func Unexport(s string) string { return strings.ToLower(s[:1]) + s[1:] }
+
+func CamelCaseVars(s string) string {
+	subs := strings.Split(s, ".")
+	vars := make([]string, 0, len(subs))
+	for _, sub := range subs {
+		vars = append(vars, CamelCase(sub))
+	}
+	return strings.Join(vars, ".")
+}
